9adc4d7a6f134b5d0445e42c6c1c54c6b7858228
unknown <905_Kyle@symphox.com>
2016-05-10 17:05:41 +0800

    test

---
+++ src/test/java/com/symphox/test/MyTest.java
@@ -0,0 +1,18 @@
+package com.symphox.test;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyTest {
+	@Test
+    public void testServlet1() throws Exception {
+        assertTrue(1 == 1);
+    }
+	
+	@Test
+    public void testServlet2() throws Exception {
+        assertTrue(1 == 1);
+    }
+}
